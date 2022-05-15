d46d1a27e104807aa08c200d73400a816bb05f04
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-05-15 13:15:29 -0400

    custom searchbar wip

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption'); 
+// const search = new Filter('search', 'data-caption'); 
+
+// JavaScript code
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('img');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
